1c37de5fc46eef6e0599e94ccbe6ae174a1c4827
Gunta2ps <158063331+Gunta2ps@users.noreply.github.com>
2024-12-12 11:32:21 +0700

    Done

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName:'JavaScript Tutorial',
+        productNumber:'845315',
+        paymentStatus:'Due',
+        status: 'Pending'
+    },
+    {
+        productName:'CSS Full Course',
+        productNumber:'423589',
+        paymentStatus:'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName:'Html Tutorial',
+        productNumber:'965102',
+        paymentStatus:'Paid',
+        status: 'Active'
+    }
+]
